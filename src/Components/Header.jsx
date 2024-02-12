d5b8f1bf86334293a694937e1fdd1bf17ffff48d
Rbrahma1245 <rbrahma1245@gmail.com>
2024-02-12 10:57:10 +0530

    added external css

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,26 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 export class Header extends Component {
   render() {
     return (
-      <div style={{ display: "flex" , background:"#1F2544", color:"white" , padding:20, marginBottom:50}}>
-        <div className="header-left" style={{width:"50%",display:"flex", gap:20}}>
-          <Link to="/" style={{color:"white", textDecoration:"none"}}>HOME</Link>
-          <Link to="/user/123 " style={{color:"white", textDecoration:"none"}}>USER</Link>
+      <div className="header-container">
+        <div className="header-left">
+          <Link to="/" className="link">
+            HOME
+          </Link>
+          <Link to="/user/123" className="link">
+            USER
+          </Link>
         </div>
-        <div className="header-right" style={{width: "50%", display:"flex", justifyContent:"end", gap:20}}>
-          <Link to="/about" style={{color:"white", textDecoration:"none"}}>ABOUT</Link>
-          <Link to="/contact" style={{color:"white", textDecoration:"none"}}>CONTACT</Link>
+        <div className="header-right">
+          <Link to="/about" className="link">
+            ABOUT
+          </Link>
+          <Link to="/contact" className="link">
+            CONTACT
+          </Link>
         </div>
       </div>
     );
